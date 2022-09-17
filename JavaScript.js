bc581ec9c8a33b92136c58f56f96a77c147fb578
Salim Shrestha <113753193+salimshre@users.noreply.github.com>
2022-09-17 11:24:50 +0545

    Create JavaScript.js

---
+++ JavaScript.js
@@ -0,0 +1,54 @@
+var UserPrompt = prompt("enter username");
+var PassPrompt= prompt("enter password");
+
+var database = [{
+	username: "salim",
+	password: "salim123"
+},
+
+{
+	username: "arya",
+	password: "stark123"
+},
+
+{
+	username: "tyrion",
+	password: "lannister"
+},
+
+{
+	username: "Daenerys",
+	password: "Targaryen"
+},
+
+{
+	username: "cersei",
+	Password: "lannister"
+},
+
+{
+	username: "kal",
+	Password: "drogo"
+},
+];
+
+
+function valid(username, password) {
+	debugger;
+	for(var i=0; i<database.length; i++){
+		if (database[i].username === username && database[i].password === password) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function signin(username, password) {
+	if (valid(username, password)){
+		alert("login successfully");
+	} else{
+		alert("wrong username and password");
+	}
+}
+
+signin(UserPrompt, PassPrompt)
